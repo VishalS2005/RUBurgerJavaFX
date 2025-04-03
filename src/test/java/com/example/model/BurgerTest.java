083a5deab209f7667c4d53b205482a3c5a44c843
wekantakabotdis <chenyiningchris@gmail.com>
2025-04-03 17:20:02 -0400

    Junit Test Cases Completed

--- src/test/java/com/example/model/BurgerTest.java
+++ src/test/java/com/example/model/BurgerTest.java
@@ -1,12 +1,37 @@
 package com.example.model;
 
-import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-class BurgerTest {
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class BurgerTest {
+
+    private Burger singleLettuceTomatoes;
+    private Burger doubleOnionsCheese;
+
+    @Before
+    public void seUp() {
+        ArrayList<AddOns> lettuceTomatoes = new ArrayList<>();
+        ArrayList<AddOns> onionsCheese = new ArrayList<>();
+        lettuceTomatoes.add(AddOns.LETTUCE);
+        lettuceTomatoes.add(AddOns.TOMATOES);
+        onionsCheese.add(AddOns.ONIONS);
+        onionsCheese.add(AddOns.CHEESE);
+        singleLettuceTomatoes = new Burger(Bread.PRETZEL, false, lettuceTomatoes);
+        doubleOnionsCheese = new Burger(Bread.WHEAT_BREAD, true, onionsCheese);
+    }
 
     @Test
-    void price() {
+    public void singleLettuceTomatoesprice() {
+       assertEquals(7.59,   singleLettuceTomatoes.price(), 0.01);
+    }
+
+    @Test
+    public void doubleOnionsCheesePrice() {
+        assertEquals(10.79,  doubleOnionsCheese.price(), 0.01);
     }
 }
--- src/test/java/com/example/model/SandwichTest.java
+++ src/test/java/com/example/model/SandwichTest.java
@@ -1,12 +1,50 @@
 package com.example.model;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Before;
+import org.junit.Test;
 
-class SandwichTest {
+import java.util.ArrayList;
+
+public class SandwichTest {
+
+    private Sandwich roastBeefLettuceTomatoesOnions;
+
+    private Sandwich salmonAvocadoCheese;
+
+    private Sandwich chickenOnionsAvocado;
+
+    @Before
+    public void setUp() {
+        ArrayList<AddOns> lettuceTomatoesOnions = new ArrayList<>();
+        ArrayList<AddOns> avocadoCheese = new ArrayList<>();
+        ArrayList<AddOns> onionsAvocado = new ArrayList<>();
+        lettuceTomatoesOnions.add(AddOns.LETTUCE);
+        lettuceTomatoesOnions.add(AddOns.TOMATOES);
+        lettuceTomatoesOnions.add(AddOns.ONIONS);
+        avocadoCheese.add(AddOns.AVOCADO);
+        avocadoCheese.add(AddOns.CHEESE);
+        onionsAvocado.add(AddOns.ONIONS);
+        onionsAvocado.add(AddOns.AVOCADO);
+        roastBeefLettuceTomatoesOnions = new Sandwich(Bread.BAGEL, Protein.ROAST_BEEF, lettuceTomatoesOnions);
+        salmonAvocadoCheese = new Sandwich(Bread.SOURDOUGH, Protein.SALMON, avocadoCheese);
+        chickenOnionsAvocado =  new Sandwich(Bread.BRIOCHE, Protein.CHICKEN, onionsAvocado);
+    }
+
 
     @Test
-    void price() {
+    public void roastBeefLettuceTomatoesOnionsPrice() {
+        assertEquals(11.89, roastBeefLettuceTomatoesOnions.price(), 0.01);
+    }
+
+    @Test
+    public void salmonAvocadoCheesePrice() {
+        assertEquals(11.49, salmonAvocadoCheese.price(), 0.01);
+    }
+
+    @Test
+    public void chickenOnionsAvocadoPrice() {
+        assertEquals(9.79,  chickenOnionsAvocado.price(), 0.01);
     }
 }
