52b81798ab5c6fd44f080ebc3205d7470a414973
VishalS2005 <vishalsaran2021@gmail.com>
2025-04-06 18:21:37 -0400

    Order Controller

---
+++ src/main/java/com/example/controller/OrderController.java
@@ -0,0 +1,62 @@
+package com.example.controller;
+
+import com.example.model.Order;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class OrderController {
+
+    NumberFormat formatter = NumberFormat.getCurrencyInstance(Locale.US);
+
+    private MainController mainController;
+
+    private Stage stage;
+
+    private Scene primaryScene;
+
+    private Stage primaryStage;
+
+    @FXML
+    private TextField tf_total;
+
+    @FXML
+    public ComboBox<String> cb_ordernum;
+
+    @FXML
+    public ListView<String> lv_order;
+
+    @FXML
+    private void initialize() {
+
+    }
+
+    /**
+     * Get the reference to the MainController object.
+     * We can call any public method defined in the controller through the reference.
+     */
+    public void setMainController (MainController controller,
+                                   Stage stage,
+                                   Stage primaryStage,
+                                   Scene primaryScene) {
+        mainController = controller;
+        this.stage = stage;
+        this.primaryStage = primaryStage;
+        this.primaryScene = primaryScene;
+    }
+
+    /**
+     * Navigate back to the main view.
+     */
+    @FXML
+    public void displayMain() {
+        //stage.close(); //close the window.
+        this.primaryStage.setScene(primaryScene);
+        this.primaryStage.show();
+    }
+}
+
