17938d1ed07b443af91b5c41a8dd53f37ea57d84
VishalS2005 <vishalsaran2021@gmail.com>
2025-04-06 01:31:39 -0400

    Burger and main Controller

---
+++ src/main/java/com/example/controller/ComboController.java
@@ -0,0 +1,47 @@
+package com.example.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.stage.Stage;
+
+public class ComboController {
+
+    private MainController mainController;
+
+    private Stage stage;
+
+    private Scene primaryScene;
+
+    private Stage primaryStage;
+
+    /**
+     * Get the reference to the MainController object.
+     * We can call any public method defined in the controller through the reference.
+     */
+    public void setMainController (MainController controller,
+                                   Stage stage,
+                                   Stage primaryStage,
+                                   Scene primaryScene) {
+        mainController = controller;
+        this.stage = stage;
+        this.primaryStage = primaryStage;
+        this.primaryScene = primaryScene;
+    }
+
+    /**
+     * Navigate back to the main view.
+     */
+
+    @FXML
+    public void displayMain() {
+        //stage.close(); //close the window.
+        this.primaryStage.setScene(primaryScene);
+        this.primaryStage.show();
+    }
+
+}
+
